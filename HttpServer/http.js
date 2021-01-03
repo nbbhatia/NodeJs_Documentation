4a0a8f9897b7fb4c2efc8d60ef6e6610d47c82a0
nidhi <nidhibhatia7078@gmail.com>
2021-01-03 21:47:58 +0530

    User api added

--- HttpServer/http.js
+++ HttpServer/http.js
@@ -1,7 +1,14 @@
+const { fstat } = require("fs");
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  res.end("hello , this is nidhi from my own server");
+  if (req.url === "/showUsers") {
+    fs.readFile(`${__dirname}/UserAPI/userapi.json`, "utf-8", (err, data) => {
+      res.end(data);
+      console.log("data", data);
+    });
+  }
 });
-server.listen(8000, () => {
+server.listen(8090, () => {
   console.log("server is runnig on 8000");
 });
