d797aad0c17b91e68047b52ed5196bd9ec473fe6
nidhi <nidhibhatia7078@gmail.com>
2021-01-23 17:17:44 +0530

    serve json data in req

--- ExpressJS/src/routing/public/serveStaticFile.js
+++ ExpressJS/src/routing/public/serveStaticFile.js
@@ -5,7 +5,12 @@ const path = require("path");
 // serve static file
 const staticPath = path.join(__dirname, "../public");
 app.use(express.static(staticPath));
-
-app.listen(8080, () => {
+app.get("/jsondata", (req, res) => {
+  res.send({
+    name: "nidhi",
+    age: 25,
+  });
+});
+app.listen(8090, () => {
   console.log("listening port at 8080");
 });
