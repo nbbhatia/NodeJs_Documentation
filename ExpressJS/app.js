9dd5be685d61077ffa67dfbac2055ff7aba69fa3
nidhi <nidhibhatia7078@gmail.com>
2021-01-23 11:52:50 +0530

    create server using express

---
+++ ExpressJS/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+// app.get(route, callback);
+app.get("/", (req, res) => {
+  res.send("hello form expres");
+});
+app.get("/about",(req,res)=>
+{
+    res.send("hello from about page")
+})
+app.listen(8080, () => {
+  console.log("listening port at 8080");
+});
